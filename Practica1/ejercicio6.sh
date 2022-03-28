3d9f002a5162305efad10f51b69009378375a01a
AnonymusRiv <carlos10.arco@gmail.com>
2022-03-28 13:00:10 +0200

    Finish Practice 1

--- Practica1/ejercicio6.sh
+++ Practica1/ejercicio6.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-usuario="$1"
-
-#abre el fichero users.txt
-if [  ] #si se encuentra el nombre $1 en el fichero
+if [ $# -ne 1 ]
 then
-    echo "Ya se encuentra el usuario $usuario"
+    echo "Argumentos incorrecto. Uso: ./ejercicio6.sh <usuario>"
     exit
-else
-    
 fi
+
+usuario="$1" #variable global
+
+crear_usuario(){
+    contador=0
+    for user in $(cat users.txt)
+    do
+        if [[ $user == $usuario ]] #si se encuentra el nombre $user en el fichero
+        then
+            echo "Ya se encuentra el usuario $usuario"
+            let "contador = contador + 1"
+            exit
+        fi
+    done
+    if [[ $contador == 0 ]]
+    then
+        echo -e $usuario >> users.txt #-e es para que coja el salto de linea  ("\n")-n es para que no meta otro salto de linea al final del fichero
+        mkdir homes/$usuario
+        cp skel/* homes/$usuario
+        echo "Se ha creado el usuario $usuario"
+    fi
+}
+
+crear_usuario
